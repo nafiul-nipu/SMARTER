9d92e9e6104ce095306e83baf12eeaa9e36764f1
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2020-08-20 01:03:13 -0500

    survminer goes crazy

--- prediction/prediction_with_radiomics.py
+++ prediction/prediction_with_radiomics.py
@@ -32,7 +32,7 @@ def output():
             # library(tidyverse)
             # library(purrr)
             if(!require("pacman")) install.packages("pacman")
-            pacman::p_load(pacman, survival, tidyverse,survminer)
+            pacman::p_load(pacman, survival, tidyverse)
 
             ### set file directory
             ## Modify this to correct file location 
@@ -412,7 +412,7 @@ def output():
             # library(tidyverse)
             # library(purrr)
             if(!require("pacman")) install.packages("pacman")
-            pacman::p_load(pacman, survival, tidyverse,survminer)
+            pacman::p_load(pacman, survival, tidyverse)
 
             ### set file directory
             ## Modify this to correct file location 
