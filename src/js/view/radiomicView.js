4d24562e19ce2b00f3c3a6dd3acbc81cc5236cb2
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2020-09-01 14:55:36 -0500

    another idea added.. possible fix needed

--- src/js/view/radiomicView.js
+++ src/js/view/radiomicView.js
@@ -22,6 +22,8 @@ let RadiomicView = function(){
 
         let name = ["FDT", "ASP", "PRG", "OS"]
         let data = [0.06494370649060044,0.0673236467699309,0.8702396477183327,0.9272890829277212]
+        let data2 = [(1 - data[0]), (1 - data[1]), (1 - data[2]), (1 - data[3])]
+        // console.log(data2)
 
         for(let i=0; i < 4; i++){
             
@@ -35,32 +37,61 @@ let RadiomicView = function(){
                 .axisBottom()
                 .scale(x)
                 .ticks(5);
-
-            // Draw the axis
-            svg
-            .append("g")
-            .attr("transform", "translate(0," + (50 + 60 * i) +")")      // This controls the vertical position of the Axis
-            .call(xAxis);  
             
             svg.append("g").append("text")
             .attr("x", 0)
             .attr("y", (60+60*i))
             .style("font-size", "10px")
             .text(name[i])           
-            
-            // Initialize circles. Note that the X scale is not available yet, so we cannot draw them
-     // Add dots
-        svg.append('g')
-        .append("circle")
-        .attr("cx", function(){
-                console.log(x(data[i]));
-                return x(data[i])
-            })
-            .attr("cy", (50 + 60 * i) )
-            .attr("r", 5)
-            .style("fill", "black")
 
-        }
+            if(i < 2 ){
+                // Draw the axis
+            svg
+            .append("g")
+            .attr("transform", "translate(0," + (50 + 60 * i) +")")      // This controls the vertical position of the Axis
+            .call(xAxis);  
+                            // Initialize circles. Note that the X scale is not available yet, so we cannot draw them
+        // Add dots
+            svg.append('g')
+            .append("circle")
+            .attr("cx", function(){
+                    // console.log(x(data[i]));
+                    return x(data[i])
+                })
+                .attr("cy", (50 + 60 * i) )
+                .attr("r", 5)
+                .style("fill", "black")
+
+        }else{
+            var rects = svg.append("rect")
+                        .attr('class', 'rect1')
+                        .attr("x", 30)
+                        .attr("y", (60+60*i))
+                        .attr("height", 10)
+                        .attr("width", function(){
+                            console.log(x(data[i]), "first rect")
+                            return x(data[i])
+                        })
+                        .attr("fill", "#e34a33");
+
+        var rects1 = svg.append("rect")
+                    .attr('class', 'rect')
+                    .attr("x", x(data[i]))
+                    .attr("y", (60+60*i))
+                    .attr("height", 10)
+                    .attr("width", function(){
+                        console.log(x(data2[i]), "second rect")
+                        return x(data2[i])
+                    })
+                    .attr("fill", "#fee8c8");
+                    
+                
+                }
+
+            
+    
+    }
+        
 
        
                 
